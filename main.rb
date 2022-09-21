9b47eea64898fe74fd7721be188ad4a3b6ebf6a1
wayungi <fwayungi@gmail.com>
2022-09-22 00:41:47 +0300

    Add morse word converter

--- main.rb
+++ main.rb
@@ -1,9 +1,44 @@
 
 # decode a morse character
 def decode_char (code)
-    morse_hash = {".-" => "A", "-..." => "B", "-.-." => "c", "-.." => "D", "." => "E"}
+    morse_hash = {  ".-" => "A", 
+                    "-..." => "B", 
+                    "-.-." => "c", 
+                    "-.." => "D", 
+                    "." => "E",
+                    "..-." => "F",
+                    "--." => "G",
+                    "...." => "H",
+                    ".." => "I",
+                    ".---" => "J",
+                    "-.-" => "K",
+                    ".-.." => "L",
+                    "--" => "M",
+                    "-." => "N",
+                    "---" => "O",
+                    ".--." => "P",
+                    "--.-" => "Q",
+                    ".-." => "R",
+                    "..." => "S",
+                    "-" => "T",
+                    "..-" => "U",
+                    "...-" => "V",
+                    ".--" => "W",
+                    "-..-" => "X",
+                    "-.--" => "Y",
+                    "--.." => "Z"
+                }
     morse_hash[code]
 end
+# puts(decode_char("-..."))
 
+def decode_word (morse)
+    word = ""
+    for code in morse.split(" ")
+        word += decode_char(code)
+    end
+    word
+    
+end
+# puts(decode_word("-... -.- -.."))
 
-puts(decode_char("-..."))
